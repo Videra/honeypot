51a7da7ecb7086f42faff7aa9e0b3f7539d9ffd3
jorge.david.garcia.prada <jorge.david.garcia.prada@student.howest.be>
2021-11-20 06:40:44 +0100

    Clean unused code

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\UploadController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
