618d0e81475689e97b466255ec24e949a37b293f
jorge.david.garcia.prada <jorge.david.garcia.prada@student.howest.be>
2021-11-21 05:31:32 +0100

    Refactor blade templates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,18 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-5">
-            <div class="card text-center">
-                <div class="card-header">Profile</div>
-
-                <div class="card-body">
-                    <img class="image rounded-circle" src="{{ asset('/'.Auth::user()->avatar)}}" alt="" style="width: 80px;height: 80px; padding: 10px; margin: 0px; ">
-                    <h5 class="card-title">{{ Auth::user()->name }}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">{{ Auth::user()->is_admin ? 'Admin' : 'User' }}</h6>
-                </div>
-
-                @include('user.upload')
-
-            </div>
+            @include('user.profile')
         </div>
     </div>
 </div>
