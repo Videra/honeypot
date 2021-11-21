8bc473dd58c4e9993b77a84b86beef5dede7f70f
jorge.david.garcia.prada <jorge.david.garcia.prada@student.howest.be>
2021-11-21 04:03:55 +0100

    Add filter for admins

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -12,10 +12,16 @@
 
                     <div class="btn-group mb-2">
                         <button disabled class="btn btn-dark">Show</button>
-                        <a href="{{ route('admin') }}" class="btn btn-outline-dark">all</a>
-                        <a href="{{ route('users.active') }}" class="btn btn-outline-dark">active</a>
-                        <a href="{{ route('users.enabled') }}" class="btn btn-outline-dark">enabled</a>
-                        <a href="{{ route('users.disabled') }}" class="btn btn-outline-dark">disabled</a>
+                        <a href="{{ route('users') }}"
+                           class="btn {{ Request::is('users') ? 'btn-dark' : 'btn-outline-dark'}}">all</a>
+                        <a href="{{ route('users.admin') }}"
+                           class="btn {{ Request::is('users/admin') ? 'btn-dark' : 'btn-outline-dark'}}">admin</a>
+                        <a href="{{ route('users.active') }}"
+                           class="btn {{ Request::is('users/active') ? 'btn-dark' : 'btn-outline-dark'}}">active</a>
+                        <a href="{{ route('users.enabled') }}"
+                           class="btn {{ Request::is('users/enabled') ? 'btn-dark' : 'btn-outline-dark'}}">enabled</a>
+                        <a href="{{ route('users.disabled') }}"
+                           class="btn {{ Request::is('users/disabled') ? 'btn-dark' : 'btn-outline-dark'}}">disabled</a>
                     </div>
 
                     <div class="table-responsive">
--- routes/web.php
+++ routes/web.php
@@ -29,10 +29,11 @@ Auth::routes([
 Route::get('home', [UserController::class, 'show'])->name('home');
 Route::post('home', [UserController::class, 'upload'])->name('upload');
 
-Route::get('admin', [AdminController::class, 'show'])->name('admin');
-Route::get('admin/active', [AdminController::class, 'showActive'])->name('users.active');
-Route::get('admin/enabled', [AdminController::class, 'showEnabled'])->name('users.enabled');
-Route::get('admin/disabled', [AdminController::class, 'showDisabled'])->name('users.disabled');
-Route::put('admin/users/{id}/enable', [AdminController::class, 'enable'])->name('users.enable');
-Route::put('admin/users/{id}/disable', [AdminController::class, 'disable'])->name('users.disable');
-Route::delete('admin/users/{id}/delete', [AdminController::class, 'delete'])->name('users.delete');
+Route::get('users', [AdminController::class, 'show'])->name('users');
+Route::get('users/admin', [AdminController::class, 'showAdmin'])->name('users.admin');
+Route::get('users/active', [AdminController::class, 'showActive'])->name('users.active');
+Route::get('users/enabled', [AdminController::class, 'showEnabled'])->name('users.enabled');
+Route::get('users/disabled', [AdminController::class, 'showDisabled'])->name('users.disabled');
+Route::put('users/{id}/enable', [AdminController::class, 'enable'])->name('users.enable');
+Route::put('users/{id}/disable', [AdminController::class, 'disable'])->name('users.disable');
+Route::delete('users/{id}/delete', [AdminController::class, 'delete'])->name('users.delete');
