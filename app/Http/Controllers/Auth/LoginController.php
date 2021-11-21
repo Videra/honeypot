57394642cf6b1208cd30c3d827446828aa83499d
jorge.david.garcia.prada <jorge.david.garcia.prada@student.howest.be>
2021-11-21 06:57:01 +0100

    Fix broken link to users

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -47,7 +47,7 @@ class LoginController extends Controller
     protected function authenticated(Request $request, $user)
     {
         if ($user->isAdmin()) {
-            return redirect('admin');
+            return redirect('users');
         }
 
         return redirect('home');
