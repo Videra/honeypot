1244cd84afbfaf91a29cd5f2d08d79b387540bdd
jorge.david.garcia.prada <jorge.david.garcia.prada@student.howest.be>
2021-11-25 21:46:33 +0100

    Register default Listeners

--- app/Http/Controllers/ChallengesController.php
+++ app/Http/Controllers/ChallengesController.php
@@ -11,7 +11,6 @@ use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class ChallengesController extends Controller
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,26 @@
 
 namespace App\Providers;
 
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use App\Listeners\LogAuthenticated;
+use App\Listeners\LogAuthenticationAttempt;
+use App\Listeners\LogCurrentDeviceLogout;
+use App\Listeners\LogFailedLogin;
+use App\Listeners\LogLockout;
+use App\Listeners\LogOtherDeviceLogout;
+use App\Listeners\LogPasswordReset;
+use App\Listeners\LogSuccessfulLogin;
+use App\Listeners\LogValidated;
+use Illuminate\Auth\Events\Attempting;
+use Illuminate\Auth\Events\Authenticated;
+use Illuminate\Auth\Events\CurrentDeviceLogout;
+use Illuminate\Auth\Events\Failed;
+use Illuminate\Auth\Events\Lockout;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Auth\Events\OtherDeviceLogout;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -15,8 +31,35 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        Authenticated::class => [
+            LogAuthenticated::class,
+        ],
+        Attempting::class => [
+            LogAuthenticationAttempt::class,
+        ],
+        Login::class => [
+            LogSuccessfulLogin::class,
+        ],
+        Failed::class => [
+            LogFailedLogin::class,
+        ],
+        Validated::class => [
+            LogValidated::class,
+        ],
+        Verified::class => [
+            LogFailedLogin::class,
+        ],
+        CurrentDeviceLogout::class => [
+            LogCurrentDeviceLogout::class,
+        ],
+        OtherDeviceLogout::class => [
+            LogOtherDeviceLogout::class,
+        ],
+        Lockout::class => [
+            LogLockout::class,
+        ],
+        PasswordReset::class => [
+            LogPasswordReset::class,
         ],
     ];
 
