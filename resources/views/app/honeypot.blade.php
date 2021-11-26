64709f397f03663b8f8a539e8b2fe06fb0b21e7f
jorge.david.garcia.prada <jorge.david.garcia.prada@student.howest.be>
2021-11-26 20:22:18 +0100

    Added count of solved challenges in welcome page

--- resources/views/app/honeypot.blade.php
+++ resources/views/app/honeypot.blade.php
@@ -8,8 +8,13 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-body text-center">
+                    @guest
                         <h5 class="card-title">Welcome, guest</h5>
                         <p class="card-text">Please login or register to access our CTF challenges</p>
+                    @else
+                        <h5 class="card-title">Welcome, {{ Auth()->user()->name }}</h5>
+                        <p class="card-text">You have solved {{ count(Auth()->user()->successes) }} challenge/s.</p>
+                    @endguest
                     </div>
                 </div>
             </div>
